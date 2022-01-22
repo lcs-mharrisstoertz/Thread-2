23ed5a2e85492a3377d6527103f7d1224a2bf9c2
Morgan <Mharrisstoertz@gmail.com>
2022-01-22 18:29:44 -0500

    gig poster 1 final copy

--- Playground.playground/Pages/Gig poster.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Gig poster.xcplaygroundpage/Contents.swift
@@ -55,14 +55,14 @@ canvas.drawAxes(withScale: true, by: 50, color: .black)
  
  */
 //add background
-canvas.fillColor = Color(hue: 193, saturation: 99, brightness: 86, alpha: 80)
+canvas.fillColor = Color(hue: 193, saturation: 99, brightness: 86, alpha: 100)
 canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 
 // draw white circles
 
 
 canvas.fillColor = .white
-canvas.drawEllipse(at: Point(x: 200, y: 400), width: 20, height: 20, borderWidth: 10)
+canvas.drawEllipse(at: Point(x: 200, y: 400), width: 10, height: 10)
 
 canvas.borderColor = .white
 
@@ -71,11 +71,47 @@ canvas.drawShapesWithBorders = true
 
 
 
-for size in stride(from: 200, through: 400, by: 30) {
+for size in stride(from: 40, through: 400 , by: 50) {
     
-    canvas.drawEllipse(at: Point(x: 200, y: 400), width: size, height: size , borderWidth: 10)
+    canvas.drawEllipse(at: Point(x: 200, y: 400), width: size, height: size , borderWidth: 12)
 }
 
+
+//draw pink circles
+canvas.drawShapesWithFill = true
+
+canvas.fillColor = Color(hue: 312, saturation: 63, brightness: 66, alpha: 100)
+
+canvas.drawEllipse(at: Point(x: 200, y: 500), width: 10, height: 10)
+
+
+canvas.borderColor = Color(hue: 312, saturation: 63, brightness: 66, alpha: 100)
+
+canvas.drawShapesWithFill = false
+canvas.drawShapesWithBorders = true
+
+
+
+for size in stride(from: 40, through: 400 , by: 50) {
+    
+    canvas.drawEllipse(at: Point(x: 200, y: 500), width: size, height: size , borderWidth: 12)
+}
+
+// add text
+
+canvas.drawText(message: "superdrag", at: Point(x: 25, y: 80), size: 40)
+
+canvas.drawText(message: "with", at: Point(x: 25, y:45), size: 8)
+canvas.drawText(message: "the shambles", at: Point(x: 25, y: 30), size: 8)
+canvas.drawText(message: "and lifter", at: Point(x: 25, y: 15), size: 8)
+
+canvas.drawText(message: "thursday", at: Point(x: 115, y:45), size: 8)
+canvas.drawText(message: "june 13 1996 / 8:30", at: Point(x: 115, y: 30), size: 8)
+canvas.drawText(message: "no age limit", at: Point(x: 115, y: 15), size: 8)
+
+canvas.drawText(message: "at brick by brick", at: Point(x: 300, y:45), size: 8)
+canvas.drawText(message: "1130 buenos avenue", at: Point(x: 300, y: 30), size: 8)
+canvas.drawText(message: "san diego, ca", at: Point(x: 300, y: 15), size: 8)
 /*:
  ## Show the Live View
  Don't see any results?
--- Playground.playground/Pages/gig poster 2.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/gig poster 2.xcplaygroundpage/Contents.swift
@@ -14,6 +14,7 @@ let preferredHeight = 600
  Please do not remove.
  */
 import Cocoa
+
 import PlaygroundSupport
 import CanvasGraphics
 
