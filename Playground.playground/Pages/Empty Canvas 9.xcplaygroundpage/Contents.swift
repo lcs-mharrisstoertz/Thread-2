a29f30d6827e8165cc545ed2a68a8cbba1a7b38a
Morgan <Mharrisstoertz@gmail.com>
2022-02-03 22:04:58 -0500

    added text on Niki Minaj poster

--- Playground.playground/Pages/Empty Canvas 9.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 9.xcplaygroundpage/Contents.swift
@@ -16,6 +16,7 @@ let preferredHeight = 600
 import Cocoa
 import PlaygroundSupport
 import CanvasGraphics
+import OpenGL
 
 // Create canvas
 let canvas = Canvas(width: preferredWidth, height: preferredHeight)
@@ -50,12 +51,15 @@ PlaygroundPage.current.liveView = canvas
  */
 canvas.drawAxes(withScale: true, by: 50, color: .black)
 
+// BACKGROUND
+canvas.fillColor = Color(hue: 320, saturation: 25, brightness: 100, alpha: 100)
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 
-for xPosition in stride(from: 0, to: 400, by: 100) {
-    for yPosition in stride(from: 60, to: 400, by: 100) {
+
+for xPosition in stride(from: 40, to: 320, by: 80) {
+    for yPosition in stride(from: 100, to: 400, by: 80) {
  
-        //hearts
-        
+        //FIRST HEART
         
         var figureVertices: [Point] = []
       //first half
@@ -75,7 +79,8 @@ for xPosition in stride(from: 0, to: 400, by: 100) {
 
         canvas.fillColor = Color(hue: 321, saturation: 90, brightness: 100, alpha: 100)
         canvas.drawCustomShape(with: figureVertices)
-        
+    
+        // SECOND HEART
         figureVertices = []
       //first half
         figureVertices.append(Point(x: xPosition + 50, y: yPosition + 30))
@@ -100,6 +105,47 @@ for xPosition in stride(from: 0, to: 400, by: 100) {
     }}
 
 // add text
+
+canvas.textColor = .black
+canvas.drawText(message: "NIKI MINAJ", at: Point(x: 50, y: 525), size: 50)
+canvas.drawText(message: "PINK FRIDAY", at: Point(x: 50, y: 475), size: 50)
+
+canvas.drawText(message: "JUNE 18, 2012", at: Point(x: 50, y: 75), size: 20)
+canvas.drawText(message: "AFAS LIVE", at: Point(x: 50, y: 50), size: 20)
+canvas.drawText(message: "AMSTERDAM, NORTH HOLLAND", at: Point(x: 50, y: 25), size: 20)
+
+canvas.textColor = Color(hue: 320, saturation: 40, brightness: 100, alpha: 100)
+canvas.drawText(message: "NIKI MINAJ", at: Point(x: 45, y: 525), size: 50)
+canvas.drawText(message: "PINK FRIDAY", at: Point(x: 45, y: 475), size: 50)
+
+// butterflies
+canvas.fillColor = .black
+// butterfly 1
+canvas.drawEllipse(at: Point(x: 350, y: 85), width: 10, height: 35)
+canvas.drawEllipse(at: Point(x: 340, y: 90), width: 15, height: 15)
+canvas.drawEllipse(at: Point(x: 340, y: 75), width: 18, height: 18)
+canvas.drawEllipse(at: Point(x: 360, y: 90), width: 15, height: 15)
+canvas.drawEllipse(at: Point(x: 360, y: 75), width: 18, height: 18)
+canvas.drawRoundedRectangle(at: Point(x: 346, y: 95), width: 2, height: 15)
+canvas.drawRoundedRectangle(at: Point(x: 350, y: 95), width: 2, height: 15)
+
+//butterfly 2
+canvas.drawEllipse(at: Point(x: 50, y: 465), width: 10, height: 35)
+canvas.drawEllipse(at: Point(x: 40, y: 470), width: 15, height: 15)
+canvas.drawEllipse(at: Point(x: 40, y: 455), width: 18, height: 18)
+canvas.drawEllipse(at: Point(x: 60, y: 470), width: 15, height: 15)
+canvas.drawEllipse(at: Point(x: 60, y: 455), width: 18, height: 18)
+canvas.drawRoundedRectangle(at: Point(x: 46, y: 475), width: 2, height: 15)
+canvas.drawRoundedRectangle(at: Point(x: 50, y: 475), width: 2, height: 15)
+
+// butterfly 3
+canvas.drawEllipse(at: Point(x: 350, y: 85), width: 10, height: 35)
+canvas.drawEllipse(at: Point(x: 340, y: 90), width: 15, height: 15)
+canvas.drawEllipse(at: Point(x: 340, y: 75), width: 18, height: 18)
+canvas.drawEllipse(at: Point(x: 360, y: 90), width: 15, height: 15)
+canvas.drawEllipse(at: Point(x: 360, y: 75), width: 18, height: 18)
+canvas.drawRoundedRectangle(at: Point(x: 346, y: 95), width: 2, height: 15)
+canvas.drawRoundedRectangle(at: Point(x: 350, y: 95), width: 2, height: 15)
 /*:
  ## Show the Live View
  Don't see any results?
